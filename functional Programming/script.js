4b3c8c36823a4995f6dc4b17f4924cc9f64f2976
Mohammad Mazharul Hoque <mhoque@ael-bd.com>
2020-04-19 23:50:28 +0600

    filter another example added

--- functional Programming/script.js
+++ functional Programming/script.js
@@ -8,3 +8,16 @@ console.log(evenNumber);
 let oddNumber = numbers.filter((number,index,fullArr)=>{return number%2 !=0 });
 console.log(oddNumber);
 
+// filter another example:
+
+let forecast = [
+    {day:'sun',sun:true},
+    {day:'mon',sun:true},
+    {day:'tue',sun:false},
+    {day:'wed',sun:true},
+    {day:'thu',sun:false},
+    {day:'fri',sun:false},
+    {day:'sat',sun:true}
+]
+let sunnyDay = forecast.filter((obj)=> {return obj.sun === true});
+console.log(sunnyDay);
