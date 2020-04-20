01d44787f4d6b7ac0deb92d28a64ee43b3c6abce
Mohammad Mazharul Hoque <mhoque@ael-bd.com>
2020-04-20 13:47:06 +0600

    filter-map-reduce

--- functional Programming/script.js
+++ functional Programming/script.js
@@ -21,3 +21,24 @@ let forecast = [
 ]
 let sunnyDay = forecast.filter((obj)=> {return obj.sun === true});
 console.log(sunnyDay);
+
+
+// map example
+let sunnyDayArr = forecast.map((isSunny)=> {return isSunny.sun});
+console.log(sunnyDayArr);
+
+let serialNumber = [1,23,4,5,6,7,8,9];
+
+let squareNumber = serialNumber.map(item=>item*item);
+console.log(squareNumber);
+
+// reduce:
+let arrayOfNumber = [1,2,3,4];
+
+// let sumOfArray= arrayOfNumber.reduce(function(sum,item){
+//     return sum += item;
+// },0)
+// console.log(sumOfArray);
+let sumOfArray = arrayOfNumber.reduce((sum,item)=> sum+=item,10);
+console.log(sumOfArray);
+
