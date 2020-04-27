b6feb31c3b8c0dcbf31cb08357dea70cdb3bc33d
Mohammad Mazharul Hoque <mhoque@ael-bd.com>
2020-04-27 16:36:40 +0600

    useful array method - map() - correction ratting to rating

--- apply function to collection of data/map.js
+++ apply function to collection of data/map.js
@@ -28,22 +28,22 @@ console.log(reversedWords);
 const movies=[{
     title:'Black Widow',
     genres:['Adventure','Actions'],
-    ratting: 7.7
+    rating: 7.7
 },
 {
     title:'No Time To Die',
     genres:['Adventure','Thriller'],
-    ratting:7.6
+    rating:7.6
 },
 {
     title:'Mulan',
     genres:['Adventure','Actions'],
-    ratting: 7.3
+    rating: 7.3
 },
 {
     title:'The Invisible Man',
     genres:['Horror','Thriller'],
-    ratting: 7.2
+    rating: 7.2
 }]
 
 const movieTitle = movies.map((title)=>{
