011fe808e6e194e31666e5eafcd2e18b89f40ff7
Mohammad Mazharul Hoque <mhoque@ael-bd.com>
2020-05-16 21:25:20 +0600

    computed properties

--- Object methods & This keyword/script.js
+++ Object methods & This keyword/script.js
@@ -1,15 +1,36 @@
 // shorthand object properties
 
-const getStats = arr=>{
+const getStats = (arr) => {
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  const sum = arr.reduce((sum, r) => sum + r);
+  const avg = sum / arr.length;
 
-    const max = Math.max(...arr);
-    const min = Math.min(...arr);
-    const sum = arr.reduce((sum,r)=> sum+r);
-    const avg = sum/ arr.length;
+  return { max, min, sum, avg };
+};
 
-    return {max,min,sum,avg}
-}
-
-const numbers = [1,2,3,4,5,6,7,8,9,10];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(getStats(numbers));
+
+// Computed property
+const role = "Admin";
+const person = "Mr. Walt";
+const role2 = "Manager";
+const person2 = "Skyler Walt"
+const team = {  
+  [role]: person,
+};
+
+// old syntax
+team[role2]=person2;
+
+
+const addProp = (obj, k, v) => {
+  return { ...obj,
+     [k]: v
+}
+}
+const res = addProp(team,'Happy',':)');
+console.log(res);
+
